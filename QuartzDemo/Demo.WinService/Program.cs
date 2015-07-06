199ec99c7e62e4d699d1498a38ed07e7873c537b
jiangyongsheng <jiangyongsheng@ppmoney.com>
2015-07-06 20:08:02 +0800

    quartz Demo

---
+++ QuartzDemo/Demo.WinService/Program.cs
@@ -0,0 +1,32 @@
+﻿using Demo.WinService.Topshelf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Demo.WinService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
+            HostFactory.Run(x =>
+            {
+                x.UseLog4Net();
+
+                x.Service<ServiceRunner>();
+
+                x.SetDescription("WHTR.代扣服务");
+                x.SetDisplayName("WHTR.代扣服务");
+                x.SetServiceName("WHTR.代扣服务");
+
+                x.EnablePauseAndContinue();
+            });
+
+        }
+    }
+}
---
+++ QuartzDemo/Demo.WinService/QuartzJobs/TestJob.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.WinService.QuartzJobs
+{
+    public sealed class TestJob : IJob
+    {
+        private readonly ILog _logger = LogManager.GetLogger("TestJob");
+        public TestJob()
+        {
+
+        }
+        public void Execute(IJobExecutionContext context)
+        {
+            _logger.InfoFormat("TestJob测试");
+        }
+    }
+}
